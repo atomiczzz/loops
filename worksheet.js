8982149625d2388b1c1aae36557e107049e21d79
Matt <mwight1986@gmail.com>
2017-02-24 12:12:37 -0600

    updated

--- worksheet.js
+++ worksheet.js
@@ -1,15 +1,48 @@
 // PART 0: Write a function called squareDance() that squares each number in an array.
-
+var squareDance = function(input){
+	var sq = []
+    for (var i=0;i<input.length;i++){
+    	sq += input[i]*input[i]
+    }
+    return sq
+}
+log(squareDance([6]))
 
 // PART 1: write a function called nicer(). 
 // It should clean up the language in its input sentence. 
 // Forbidden words include heck, dang, crappy, and dang.
+var nicer = function(input){
+    var wholeSentence = ''
+    var inputArray = input.split(' ')
+	for(var i=0;i<inputArray.length; i++){
+        if(inputArray[i] === 'heck'||inputArray[i] === 'dang'||inputArray[i] ==='crappy'||inputArray[i] === 'darn'){
+        	inputArray.splice(i,1)
+        }
+    
+    }
+    return inputArray.join(' ')
+}
+log(nicer('i like got darn fried rice'))
 
 
 // // PART 2: write a function called capitalizeAll(). 
 // It should take as input a sentence and capitalize the first letter
 // of every word in the sentence. 
+var upperCase = function(input){
+	var finalInput = input.charAt(0).toUpperCase() + input.slice(1)
+	return finalInput
+ }
 
+function capitalizeEachWord(str) {
+	var newString = str.split(' ')
+    for(var i = 0;i<newString.length;i++){
+    var applyCap += newString[i].concat('')
+		//log(upperCase(newString[i]))
+    }
+    return applyCap
+}
+
+log(capitalizeEachWord('will the real slim shady please stand up'))
 
 // // PART 3: write a function called properSentences(). It should take as input a string and capitalize the first letter of every sentence in that string. (For our purposes, all sentences will end with periods. Write one that works with ? and ! and receive a gratifying high five, right on the hand!)
 
